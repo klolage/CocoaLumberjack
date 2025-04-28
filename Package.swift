b43808015285565c14a4e484ae8b1fddab94b002
klolage <161434484+klolage@users.noreply.github.com>
2025-04-28 21:25:40 +0530

    Update Package.swift - use only iOS platform
    removed other platform but iOS

--- Package.swift
+++ Package.swift
@@ -17,11 +17,7 @@ let swiftSettings: Array<SwiftSetting> = [
 let package = Package(
     name: "CocoaLumberjack",
     platforms: [
-        .macOS(.v10_13),
-        .iOS(.v12),
-        .tvOS(.v12),
-        .watchOS(.v5),
-        .visionOS(.v1),
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
